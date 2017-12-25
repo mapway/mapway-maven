e224d63cf087d4377a78ee1f2b21db8fe6da0bfe
zhangjsf <zhangjsf@enn.cn>
2017-12-25 15:50:22 +0800

    add button privileges document

--- mapway-web-frame/src/main/java/cn/ennwifi/webframe/ui/client/modules/authority/authoritydefine/EditAuthority.java
+++ mapway-web-frame/src/main/java/cn/ennwifi/webframe/ui/client/modules/authority/authoritydefine/EditAuthority.java
@@ -1,6 +1,5 @@
 package cn.ennwifi.webframe.ui.client.modules.authority.authoritydefine;
 
-
 import cn.ennwifi.webframe.ui.client.ClientContext;
 import cn.ennwifi.webframe.ui.client.common.ImageUploader;
 import cn.ennwifi.webframe.ui.client.rpc.WebFrameProxy;
@@ -28,7 +27,6 @@ import com.ksyzt.gwt.client.common.MessageComposite;
 import com.ksyzt.gwt.client.event.MessageEvent;
 import com.ksyzt.gwt.client.event.MessageHandler;
 
-
 /**
  * 编辑权限点.
  *
@@ -36,298 +34,331 @@ import com.ksyzt.gwt.client.event.MessageHandler;
  */
 public class EditAuthority extends MessageComposite {
 
-  /** The m authority. */
-  S_RESOURCEObj mAuthority;
+    /**
+     * The m authority.
+     */
+    S_RESOURCEObj mAuthority;
 
-  /** The txt NAME. */
-  @UiField
-  TextBox txtName;
+    /**
+     * The txt NAME.
+     */
+    @UiField
+    TextBox txtName;
 
-  /** The txt VALUE. */
-  @UiField
-  TextBox txtValue;
+    /**
+     * The txt VALUE.
+     */
+    @UiField
+    TextBox txtValue;
 
-  /** The txt PATH. */
-  @UiField
-  TextBox txtPath;
+    /**
+     * The txt PATH.
+     */
+    @UiField
+    TextBox txtPath;
 
-  /** The txt STYLE. */
-  @UiField
-  TextBox txtStyle;
+    /**
+     * The txt STYLE.
+     */
+    @UiField
+    TextBox txtStyle;
 
-  /** The txt RANK. */
-  @UiField
-  TextBox txtRank;
+    /**
+     * The txt RANK.
+     */
+    @UiField
+    TextBox txtRank;
 
-  /** The txt SUMMARY. */
-  @UiField
-  TextArea txtSummary;
+    /**
+     * The txt SUMMARY.
+     */
+    @UiField
+    TextArea txtSummary;
 
-  /** The ddl MODULE. */
-  @UiField
-  ListBox ddlModule;
+    /**
+     * The ddl MODULE.
+     */
+    @UiField
+    ListBox ddlModule;
 
-  /** The btn create. */
-  @UiField
-  Button btnCreate;
+    /**
+     * The btn create.
+     */
+    @UiField
+    Button btnCreate;
 
-  /** The btn delete. */
-  @UiField
-  Button btnDelete;
+    /**
+     * The btn delete.
+     */
+    @UiField
+    Button btnDelete;
 
-  /** The btn OK. */
-  @UiField
-  Button btnOk;
+    /**
+     * The btn OK.
+     */
+    @UiField
+    Button btnOk;
 
-  /** The icon. */
-  @UiField
-  ImageUploader icon;
+    /**
+     * The icon.
+     */
+    @UiField
+    ImageUploader icon;
 
-  @UiField
-  HorizontalPanel tools;
+    @UiField
+    HorizontalPanel tools;
 
-  /** The icon handler. */
-  private MessageHandler iconHandler = new MessageHandler() {
+    /**
+     * The icon handler.
+     */
+    private MessageHandler iconHandler = new MessageHandler() {
 
-    @Override
-    public void onMessage(Object sender, Integer message, Object value) {
-      if (message == MessageEvent.OK) {
-        UploadFileReturn r = (UploadFileReturn) value;
-        if (mAuthority != null) {
-          mAuthority.setIcon(r.relPath);
+        @Override
+        public void onMessage(Object sender, Integer message, Object value) {
+            if (message == MessageEvent.OK) {
+                UploadFileReturn r = (UploadFileReturn) value;
+                if (mAuthority != null) {
+                    mAuthority.setIcon(r.relPath);
+                }
+                icon.setUrl(ClientContext.getContext().getConfigure().getImagePrefix() + r.relPath);
+                icon.setImageTitle(r.relPath);
+            }
         }
-        icon.setUrl(ClientContext.getContext().getConfigure().getImagePrefix() + r.relPath);
-        icon.setImageTitle(r.relPath);
-      }
+    };
+
+    /**
+     * The ui binder.
+     */
+    private static EditAuthorityUiBinder uiBinder = GWT.create(EditAuthorityUiBinder.class);
+
+    /**
+     * The Interface EditAuthorityUiBinder.
+     */
+    interface EditAuthorityUiBinder extends UiBinder<Widget, EditAuthority> {
     }
-  };
 
-  /** The ui binder. */
-  private static EditAuthorityUiBinder uiBinder = GWT.create(EditAuthorityUiBinder.class);
-
-  /**
-   * The Interface EditAuthorityUiBinder.
-   */
-  interface EditAuthorityUiBinder extends UiBinder<Widget, EditAuthority> {
-  }
-
-  /**
-   * Instantiates a new edits the authority.
-   */
-  public EditAuthority() {
-    initWidget(uiBinder.createAndBindUi(this));
-    btnOk.setEnabled(false);
-    icon.setAction(ImageUploader.DEFAULT_ACTION, "func");
-    icon.addMessageHandler(iconHandler);
-    icon.setPixelSize(256, 180);
-    populateModules();
-  }
-
-  /**
-   * Populate modules.
-   */
-  private void populateModules() {
-    ModuleFactory proxy = BaseAbstractModule.getModuleFactory();
-    ModuleInfo[] items = proxy.getModules();
-    sort(items);
-    for (int i = 0; i < items.length; i++) {
-      ModuleInfo item = items[i];
-      ddlModule.addItem(item.group + "/" + item.name, item.code);
+    /**
+     * Instantiates a new edits the authority.
+     */
+    public EditAuthority() {
+        initWidget(uiBinder.createAndBindUi(this));
+        btnOk.setEnabled(false);
+        icon.setAction(ImageUploader.DEFAULT_ACTION, "func");
+        icon.addMessageHandler(iconHandler);
+        icon.setPixelSize(256, 180);
+        populateModules();
     }
-  }
 
-  /**
-   * 冒泡排序
-   * @param items
-   */
-  private void sort(ModuleInfo[] items) {
-    if (items == null) {
-      return;
-    }
-    ModuleInfo temp = null;
-    for (int i = 0; i < items.length - 1; i++) {
-      for (int j = i + 1; j < items.length; j++) {
-        if (items[i].group == null || items[j].group == null) {
-          continue;
+    /**
+     * Populate modules.
+     */
+    private void populateModules() {
+        ModuleFactory proxy = BaseAbstractModule.getModuleFactory();
+        ModuleInfo[] items = proxy.getModules();
+        sort(items);
+        for (int i = 0; i < items.length; i++) {
+            ModuleInfo item = items[i];
+            ddlModule.addItem(item.group + "/" + item.name, item.code);
         }
-        if (items[i].group.compareTo(items[j].group) > 0) {
-          temp = items[i];
-          items[i] = items[j];
-          items[j] = temp;
+    }
+
+    /**
+     * 冒泡排序
+     *
+     * @param items
+     */
+    private void sort(ModuleInfo[] items) {
+        if (items == null) {
+            return;
         }
-      }
+        ModuleInfo temp = null;
+        for (int i = 0; i < items.length - 1; i++) {
+            for (int j = i + 1; j < items.length; j++) {
+                if (items[i].group == null || items[j].group == null) {
+                    continue;
+                }
+                if (items[i].group.compareTo(items[j].group) > 0) {
+                    temp = items[i];
+                    items[i] = items[j];
+                    items[j] = temp;
+                }
+            }
+        }
+
     }
 
-  }
-
-  /**
-   * Edits the authority.
-   *
-   * @param authority the authority
-   */
-  public void editAuthority(S_RESOURCEObj authority) {
-    toUserInterface(authority);
-    msg("");
-  }
-
-  /**
-   * To UI.
-   *
-   * @param authority the authority
-   */
-  private void toUserInterface(S_RESOURCEObj resource) {
-    if (resource == null) {
-      resource = new S_RESOURCEObj();
-      if (mAuthority != null) {
-        resource.setPath(mAuthority.getPath());
-        resource.setPid(mAuthority.getPid());
-      }
-      if (resource.getPid() == null) {
-        resource.setPid(0L);
-      }
-      resource.setIcon("");
-      resource.setRank(1);
-      resource.setClick((long) 0);
-      resource.setRootid(null);
-      mAuthority = resource;
-    } else {
-      mAuthority = resource;
+    /**
+     * Edits the authority.
+     *
+     * @param authority the authority
+     */
+    public void editAuthority(S_RESOURCEObj authority) {
+        toUserInterface(authority);
+        msg("");
     }
 
-    txtName.setValue(mAuthority.getName());
-    txtPath.setText(mAuthority.getPath());
-    txtStyle.setValue(mAuthority.getStyle());
-    txtSummary.setValue(mAuthority.getSummary());
-    txtRank.setValue(mAuthority.getRank() + "");
+    /**
+     * To UI.
+     *
+     * @param authority the authority
+     */
+    private void toUserInterface(S_RESOURCEObj resource) {
+        if (resource == null) {
+            resource = new S_RESOURCEObj();
+            if (mAuthority != null) {
+                resource.setPath(mAuthority.getPath());
+                resource.setPid(mAuthority.getPid());
+                resource.setRootid(mAuthority.getRootid());
+            }
+            if (resource.getPid() == null) {
+                resource.setPid(0L);
+                resource.setRootid(null);
+            }
+            resource.setIcon("");
+            resource.setRank(1);
+            resource.setClick((long) 0);
 
-    for (int i = 0; i < ddlModule.getItemCount(); i++) {
-      if (ddlModule.getValue(i).equals(mAuthority.getCode())) {
-        ddlModule.setSelectedIndex(i);
-        break;
-      }
+            mAuthority = resource;
+        } else {
+            mAuthority = resource;
+        }
+
+        txtName.setValue(mAuthority.getName());
+        txtPath.setText(mAuthority.getPath());
+        txtStyle.setValue(mAuthority.getStyle());
+        txtSummary.setValue(mAuthority.getSummary());
+        txtRank.setValue(mAuthority.getRank() + "");
+
+        for (int i = 0; i < ddlModule.getItemCount(); i++) {
+            if (ddlModule.getValue(i).equals(mAuthority.getCode())) {
+                ddlModule.setSelectedIndex(i);
+                break;
+            }
+        }
+
+        txtValue.setValue(mAuthority.getPara());
+
+        if (mAuthority.getIcon() == null || mAuthority.getIcon().length() == 0) {
+            icon.setUrl(ImageUploader.EMPTY_PICTURE);
+        } else {
+            icon.setUrl(mAuthority.getIcon());
+        }
+
+        btnDelete.setEnabled(mAuthority.getId() != null);
+        btnOk.setEnabled(true);
+        msg("");
     }
 
-    txtValue.setValue(mAuthority.getPara());
+    /**
+     * The lb message.
+     */
+    @UiField
+    Label lbMessage;
 
-    if (mAuthority.getIcon() == null || mAuthority.getIcon().length() == 0) {
-      icon.setUrl(ImageUploader.EMPTY_PICTURE);
-    } else {
-      icon.setUrl(mAuthority.getIcon());
+    /**
+     * Msg.
+     *
+     * @param text the text
+     */
+    private void msg(String text) {
+        lbMessage.setText(text);
     }
 
-    btnDelete.setEnabled(mAuthority.getId() != null);
-    btnOk.setEnabled(true);
-    msg("");
-  }
+    /**
+     * On save.
+     *
+     * @param ev the ev
+     */
+    @UiHandler("btnOk")
+    void onSave(ClickEvent ev) {
+        fromUserInterface();
+        msg("开始保存...");
+        WebFrameProxy.get().saveMenu(mAuthority, new AsyncCallback<S_RESOURCEObj>() {
 
-  /** The lb message. */
-  @UiField
-  Label lbMessage;
+            @Override
+            public void onSuccess(S_RESOURCEObj result) {
+                editAuthority(result);
+                MessageEvent ev = new MessageEvent(MessageEvent.REFRESH, result);
+                fireEvent(ev);
+                msg("保存成功");
+            }
 
-  /**
-   * Msg.
-   *
-   * @param text the text
-   */
-  private void msg(String text) {
-    lbMessage.setText(text);
-  }
-
-  /**
-   * On save.
-   *
-   * @param ev the ev
-   */
-  @UiHandler("btnOk")
-  void onSave(ClickEvent ev) {
-    fromUserInterface();
-    msg("开始保存...");
-    WebFrameProxy.get().saveMenu(mAuthority, new AsyncCallback<S_RESOURCEObj>() {
-
-      @Override
-      public void onSuccess(S_RESOURCEObj result) {
-        editAuthority(result);
-        MessageEvent ev = new MessageEvent(MessageEvent.REFRESH, result);
-        fireEvent(ev);
-        msg("保存成功");
-      }
-
-      @Override
-      public void onFailure(Throwable caught) {
-        ClientContext.getContext().processFailure(caught);
-        message(caught.getMessage());
-        msg(caught.getMessage());
-      }
-    });
-  }
-
-  /**
-   * From UI.
-   */
-  private void fromUserInterface() {
-    mAuthority.setName(txtName.getValue());
-    mAuthority.setPath(txtPath.getValue());
-    mAuthority.setCode(ddlModule.getSelectedValue());
-    mAuthority.setSummary(txtSummary.getValue());
-    mAuthority.setStyle(txtStyle.getValue());
-    mAuthority.setRank(Integer.parseInt(txtRank.getValue()));
-    mAuthority.setPara(txtValue.getValue());
-    mAuthority.setRootid(Long.valueOf(1));
-  }
-
-  /**
-   * On create.
-   *
-   * @param ev the ev
-   */
-  @UiHandler("btnCreate")
-  void onCreate(ClickEvent ev) {
-    if (mAuthority != null && mAuthority.getId() != null) {
-      S_RESOURCEObj m = new S_RESOURCEObj();
-      m.setPid(mAuthority.getId());
-      m.setPath(mAuthority.getPath() + "/" + mAuthority.getName());
-      m.setRank(1);
-      m.setPara("");
-      m.setIcon("");
-      m.setRootid(mAuthority.getRootid());
-      editAuthority(m);
-    } else {
-      editAuthority(null);
-    }
-  }
-
-  /**
-   * On delete click.
-   *
-   * @param ev the ev
-   */
-  @UiHandler("btnDelete")
-  void onDeleteClick(ClickEvent ev) {
-    ClientContext.getContext().confirm("操作提示", "确认删除", new Callback<Void, Void>() {
-
-      @Override
-      public void onSuccess(Void result) {
-        WebFrameProxy.get().deleteResource(mAuthority.getId(), new AsyncCallback<Void>() {
-
-          @Override
-          public void onSuccess(Void result) {
-            editAuthority(null);
-            MessageEvent ev = new MessageEvent(MessageEvent.REFRESH, null);
-            fireEvent(ev);
-          }
-
-          @Override
-          public void onFailure(Throwable caught) {
-            ClientContext.getContext().processFailure(caught);
-            message(caught.getMessage());
-          }
+            @Override
+            public void onFailure(Throwable caught) {
+                ClientContext.getContext().processFailure(caught);
+                message(caught.getMessage());
+                msg(caught.getMessage());
+            }
         });
-      }
+    }
 
-      @Override
-      public void onFailure(Void reason) {
+    /**
+     * From UI.
+     */
+    private void fromUserInterface() {
+        mAuthority.setName(txtName.getValue());
+        mAuthority.setPath(txtPath.getValue());
+        mAuthority.setCode(ddlModule.getSelectedValue());
+        mAuthority.setSummary(txtSummary.getValue());
+        mAuthority.setStyle(txtStyle.getValue());
+        mAuthority.setRank(Integer.parseInt(txtRank.getValue()));
+        mAuthority.setPara(txtValue.getValue());
 
-      }
-    });
-  }
+    }
+
+    /**
+     * On create.
+     *
+     * @param ev the ev
+     */
+    @UiHandler("btnCreate")
+    void onCreate(ClickEvent ev) {
+        if (mAuthority != null && mAuthority.getId() != null) {
+            S_RESOURCEObj m = new S_RESOURCEObj();
+            m.setPid(mAuthority.getId());
+            m.setPath(mAuthority.getPath() + "/" + mAuthority.getName());
+            m.setRank(1);
+            m.setPara("");
+            m.setIcon("");
+            m.setRootid(mAuthority.getRootid());
+            editAuthority(m);
+        } else {
+            editAuthority(null);
+        }
+    }
+
+    /**
+     * On delete click.
+     *
+     * @param ev the ev
+     */
+    @UiHandler("btnDelete")
+    void onDeleteClick(ClickEvent ev) {
+        ClientContext.getContext().confirm("操作提示", "确认删除", new Callback<Void, Void>() {
+
+            @Override
+            public void onSuccess(Void result) {
+                WebFrameProxy.get().deleteResource(mAuthority.getId(), new AsyncCallback<Void>() {
+
+                    @Override
+                    public void onSuccess(Void result) {
+                        editAuthority(null);
+                        MessageEvent ev = new MessageEvent(MessageEvent.REFRESH, null);
+                        fireEvent(ev);
+                    }
+
+                    @Override
+                    public void onFailure(Throwable caught) {
+                        ClientContext.getContext().processFailure(caught);
+                        message(caught.getMessage());
+                    }
+                });
+            }
+
+            @Override
+            public void onFailure(Void reason) {
+
+            }
+        });
+    }
 }
