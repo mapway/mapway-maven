7ef6f5b32185bca0cfe41a55d5076009bfebf2a2
zhangjsf <zhangjsf@enn.cn>
2018-01-11 04:30:58 +0800

    change meta data editor style and hsi

--- mapway-web-frame/src/main/java/cn/ennwifi/webframe/ui/client/modules/meta/MetaTree.java
+++ mapway-web-frame/src/main/java/cn/ennwifi/webframe/ui/client/modules/meta/MetaTree.java
@@ -8,6 +8,7 @@ import cn.ennwifi.webframe.ui.client.data.LocalStorage;
 import cn.ennwifi.webframe.ui.client.rpc.WebFrameProxy;
 import cn.ennwifi.webframe.ui.shared.repository.S_METAObj;
 
+import cn.mapway.ui.client.widget.common.IconItem;
 import com.google.gwt.event.logical.shared.CloseEvent;
 import com.google.gwt.event.logical.shared.CloseHandler;
 import com.google.gwt.event.logical.shared.OpenEvent;
@@ -19,110 +20,109 @@ import com.google.gwt.user.client.ui.TreeItem;
 
 /**
  * 元数据树
- * 
- * @author zhangjianshe
  *
+ * @author zhangjianshe
  */
 public class MetaTree extends Tree implements OpenHandler<TreeItem>, CloseHandler<TreeItem> {
 
 
-  protected List<S_METAObj> mData;
+    protected List<S_METAObj> mData;
 
-  public MetaTree() {
-    this.addOpenHandler(this);
-    this.addCloseHandler(this);
-  }
-
-  public void loadData() {
-    WebFrameProxy.get().fetchMetaData(new AsyncCallback<List<S_METAObj>>() {
-
-      @Override
-      public void onSuccess(List<S_METAObj> result) {
-        mData = result;
-        refreshView();
-      }
-
-      @Override
-      public void onFailure(Throwable caught) {
-        MetaTree.this.clear();
-        MetaTree.this.add(new Label(caught.getMessage()));
-      }
-    });
-  }
-
-
-
-  protected void refreshView() {
-    if (mData == null) {
-      return;
+    public MetaTree() {
+        this.addOpenHandler(this);
+        this.addCloseHandler(this);
     }
-    this.clear();
-    HashMap<Integer, TreeItem> mapper = new HashMap<Integer, TreeItem>();
-    TreeItem root = new TreeItem();
-    root.setText("元数据");
-    root.setUserObject(null);
-    this.addItem(root);
-    for (S_METAObj meta : mData) {
-      TreeItem parent = findParent(mapper, meta);
-      if (parent == null) {
-        parent = root;
-      }
-      TreeItem item = new TreeItem();
-      item.setText(meta.getName() + "-" + meta.getCode());
-      parent.addItem(item);
-      item.setUserObject(meta);
-      mapper.put(meta.getId(), item);
-      parent.setState(isOpen("meta_" + meta.getPid()));
 
+    public void loadData() {
+        WebFrameProxy.get().fetchMetaData(new AsyncCallback<List<S_METAObj>>() {
+
+            @Override
+            public void onSuccess(List<S_METAObj> result) {
+                mData = result;
+                refreshView();
+            }
+
+            @Override
+            public void onFailure(Throwable caught) {
+                MetaTree.this.clear();
+                MetaTree.this.add(new Label(caught.getMessage()));
+            }
+        });
     }
-    root.setState(true);
-  }
 
-  private TreeItem findParent(Map<Integer, TreeItem> mapper, S_METAObj meta) {
-    if (meta == null || meta.getPid() == null) {
-      return null;
+
+    protected void refreshView() {
+        if (mData == null) {
+            return;
+        }
+        this.clear();
+        HashMap<Integer, TreeItem> mapper = new HashMap<Integer, TreeItem>();
+
+        for (S_METAObj meta : mData) {
+            TreeItem parent = findParent(mapper, meta);
+
+            TreeItem item = new TreeItem();
+            IconItem iconItem = new IconItem();
+            iconItem.setText(meta.getName() + "-" + meta.getCode());
+            item.setWidget(iconItem);
+            item.setUserObject(meta);
+            mapper.put(meta.getId(), item);
+            if (parent == null) {
+                this.addItem(item);
+                item.setState(true);
+            } else {
+                parent.addItem(item);
+                parent.setState(isOpen("meta_" + meta.getPid()));
+            }
+
+        }
     }
-    TreeItem item = mapper.get(meta.getPid());
-    return item;
-  }
 
-  @Override
-  public void onClose(CloseEvent<TreeItem> event) {
-    S_METAObj g = (S_METAObj) event.getTarget().getUserObject();
-    if (g != null) {
-      setOpen("meta_" + g.getId() + "", false);
+    private TreeItem findParent(Map<Integer, TreeItem> mapper, S_METAObj meta) {
+        if (meta == null || meta.getPid() == null) {
+            return null;
+        }
+        TreeItem item = mapper.get(meta.getPid());
+        return item;
     }
-  }
 
-  @Override
-  public void onOpen(OpenEvent<TreeItem> event) {
-    S_METAObj g = (S_METAObj) event.getTarget().getUserObject();
-    if (g != null) {
-      setOpen("meta_" + g.getId() + "", true);
+    @Override
+    public void onClose(CloseEvent<TreeItem> event) {
+        S_METAObj g = (S_METAObj) event.getTarget().getUserObject();
+        if (g != null) {
+            setOpen("meta_" + g.getId() + "", false);
+        }
     }
-  }
 
-  /**
-   * Sets the open.
-   *
-   * @param key the key
-   * @param open the open
-   */
-  public void setOpen(String key, boolean open) {
-    LocalStorage.save(key, open ? "1" : "0");
-  }
-
-  /**
-   * Checks if is open.
-   *
-   * @param key the key
-   * @return true, if is open
-   */
-  public boolean isOpen(String key) {
-    String data = LocalStorage.val(key);
-    if ("1".equals(data)) {
-      return true;
+    @Override
+    public void onOpen(OpenEvent<TreeItem> event) {
+        S_METAObj g = (S_METAObj) event.getTarget().getUserObject();
+        if (g != null) {
+            setOpen("meta_" + g.getId() + "", true);
+        }
     }
-    return false;
-  }
+
+    /**
+     * Sets the open.
+     *
+     * @param key  the key
+     * @param open the open
+     */
+    public void setOpen(String key, boolean open) {
+        LocalStorage.save(key, open ? "1" : "0");
+    }
+
+    /**
+     * Checks if is open.
+     *
+     * @param key the key
+     * @return true, if is open
+     */
+    public boolean isOpen(String key) {
+        String data = LocalStorage.val(key);
+        if ("1".equals(data)) {
+            return true;
+        }
+        return false;
+    }
 }
