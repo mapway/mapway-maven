91c46d9a73fc819a793890e0d73eec7541cbd5d0
zhangjianshe <zhangjianshe@gmail.com>
2018-05-21 20:12:55 +0800

    DOC API ADD CSS SUPPORT

--- mapway-doc-ui/src/main/java/cn/mapway/document/parser/CompileVersion.java
+++ mapway-doc-ui/src/main/java/cn/mapway/document/parser/CompileVersion.java
@@ -1,5 +1,5 @@
 package cn.mapway.document.parser;
 public class CompileVersion {
 public final static String VERSION =
-                                                    "[2.0.9]@2018年05月20日 13:41:04 from ${env.BRANCH}";
+                                                    "[2.0.8]@2018年05月21日 18:19:03 from ${env.BRANCH}";
 }
