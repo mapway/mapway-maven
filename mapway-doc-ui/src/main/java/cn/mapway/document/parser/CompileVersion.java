ee4b5969098af993d0c126b0396b71ce5902f2cf
zhangjianshe <zhangjsf@enn.cn>
2018-09-05 07:49:43 +0800

    change to sso link

--- mapway-doc-ui/src/main/java/cn/mapway/document/parser/CompileVersion.java
+++ mapway-doc-ui/src/main/java/cn/mapway/document/parser/CompileVersion.java
@@ -1,5 +1,5 @@
 package cn.mapway.document.parser;
 public class CompileVersion {
 public final static String VERSION =
-                                                    "[2.2.0]@2018年09月05日 07:22:52";
+                                                    "[2.2.1]@2018年09月05日 07:48:47";
 }
